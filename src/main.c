9ebfe763c99904986dc5bb5997c39e0c690f8830
Min Wei <mwei@lab3r7s2.42abudhabi.ae>
2026-06-22 13:07:44 +0400

    fix main()

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: weimin <weimin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mwei <mwei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 16:48:21 by weimin            #+#    #+#             */
-/*   Updated: 2026/06/22 11:19:46 by weimin           ###   ########.fr       */
+/*   Updated: 2026/06/22 13:02:56 by mwei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,24 @@ t_command *mock_parser()
 
 int main(int argc, char **argv, char **envp)
 {
-// 1. Get the first command (ls fake_file)
+    // 1. Declare the environment list variable (fixes the 'undeclared identifier' error)
+    t_env *my_env;
+
+    // 2. Silence the compiler warnings for unused variables
+    (void)argc;
+    (void)argv;
+
+    // 3. Catch signals immediately so the shell is protected
+    setup_signals();
+
+    // 4. Initialize your dynamic environment list
+    my_env = init_env(envp);
+
+    // 5. Get the first command (ls fake_file)
     t_command *cmd1 = mock_parser();
     execute_pipeline(cmd1, envp, &my_env); 
 
-    // 2. Simulate typing a second command (env)
+    // 6. Simulate typing a second command (env)
     t_command *cmd2 = malloc(sizeof(t_command));
     cmd2->args = malloc(sizeof(char *) * 2); 
     cmd2->args[0] = "env";
@@ -61,6 +74,8 @@ int main(int argc, char **argv, char **envp)
     cmd2->heredoc = NULL;
     cmd2->next = NULL;
     
-    // Execute the second command
+    // 7. Execute the second command
     execute_pipeline(cmd2, envp, &my_env);
+
+    return (0);
 }
