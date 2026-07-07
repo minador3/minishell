be3252a6754dc43ed0d2489c209ca96d1550397e
Min Wei <mwei@lab3r7s2.42abudhabi.ae>
2026-07-07 18:05:24 +0400

    redirection echo fix

--- src/execution/execute.c
+++ src/execution/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: weimin <weimin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mwei <mwei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 15:26:53 by mwei              #+#    #+#             */
-/*   Updated: 2026/07/07 03:46:11 by weimin           ###   ########.fr       */
+/*   Updated: 2026/07/07 17:58:07 by mwei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,24 @@
 static int	handle_parent_builtin(t_cmd *cmd, t_env **env, char **envp,
 		int prev)
 {
+	int original_stdout;
+	int original_stdin;
 	if (!cmd->next && prev == -1 && cmd->argv && is_builtin(cmd->argv[0]))
 	{
+		// 1. Save original file descriptors
+		original_stdout = dup(STDOUT_FILENO);
+		original_stdin = dup(STDIN_FILENO);
+		// 2. Apply ALL redirections (input and output)
+		handle_redirections(cmd, *env); 
+		// 3. Execute the builtin
 		update_exit_status(env, execute_builtin(cmd, envp, env));
+		// 4. Restore original file descriptors
+		dup2(original_stdout, STDOUT_FILENO);
+		dup2(original_stdin, STDIN_FILENO);
+		// 5. Close the duplicates
+		close(original_stdout);
+		close(original_stdin);
+		
 		free_envp_array(envp);
 		return (1);
 	}
